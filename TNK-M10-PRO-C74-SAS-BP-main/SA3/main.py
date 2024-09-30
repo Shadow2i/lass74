ca157c4bbb08147ce5762942c05d6ff8f2ef60ce
Shadow <prasoonagrawal65@gmail.com>
2024-09-30 17:56:54 +0530

    Add files via upload

---
+++ TNK-M10-PRO-C74-SAS-BP-main/SA3/main.py
@@ -0,0 +1,48 @@
+import numpy as np
+import os
+import cv2
+from cvzone.FaceDetectionModule import FaceDetector
+
+
+path = "Face-image-dataset-small"
+images = []
+ages = []
+
+detector = FaceDetector()
+
+for img in os.listdir(path):
+    try:
+        print(img)
+        if img!='.git':
+            age = img.split("_")[0]
+            ages.append(age)
+            img = cv2.imread(str(path)+"/"+str(img))
+            img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+            
+            # Detect the face in the image and store the result in img, bbox variables
+            img, bbox = detector.findFaces(img, draw=False)
+ 
+            # if bbox exits then
+            if bbox:
+                # Get the X,Y,W,H of bbox in respective variables i.e X, Y, W, H
+                X = bbox[0]['bbox'][0]
+                Y = bbox[0]['bbox'][1]
+                W = bbox[0]['bbox'][2]
+                
+                # Crop the face out of the image
+                
+                # Resize the image to 200, 200
+                        
+                # Append resized image to images array  
+
+                
+    except:
+        print("error in reading")
+
+ages = np.array(ages,dtype=np.int64)
+
+# Convert images to np.array
+
+print("Age",ages)
+
+# Print images array
